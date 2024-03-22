808cc83c2fcadaec00a9e0eafdb667e58e9d1303
Dhaedra <dhaedra@bk.ru>
2024-03-22 20:25:36 +0300

    solving a problem with comments

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode();
-        root.setValue(5);
-        TreeNode node1 = new TreeNode();
-        node1.setValue(10);
-        TreeNode node2 = new TreeNode();
-        node2.setValue(11);
-        TreeNode node3 = new TreeNode();
-        node3.setValue(12);
-        TreeNode node4 = new TreeNode();
-        node4.setValue(13);
-        TreeNode node5 = new TreeNode();
-        node5.setValue(14);
-        TreeNode node6 = new TreeNode();
-        node6.setValue(113);
-        TreeNode node7 = new TreeNode();
-        node7.setValue(10);
-
-        root.setChildren(Arrays.asList(node1,node2,node3));
-        node3.setChildren(Arrays.asList(node4,node5));
-        node5.setChildren(Arrays.asList(node6,node7));
-
-        ClassForSearchFromTreeGreaterThanN searcher = new ClassForSearchFromTreeGreaterThanN();
-        searcher.findNodesWithValueGreaterThanN(root,12);
-        System.out.println(searcher.findNodesWithValueGreaterThanN(root,12));
+        int centerX_mm = 0;
+        int centerY_mm = 0;
+        int diameter_mm = 25;
+        int resolutionDpi = 150;
+        List<int[]> circlePoints = CircleDrawer.drawCircle(centerX_mm, centerY_mm, diameter_mm, resolutionDpi);
+        for (int[] point : circlePoints) {
+            System.out.println(Arrays.toString(point));
+        }
     }
 }
